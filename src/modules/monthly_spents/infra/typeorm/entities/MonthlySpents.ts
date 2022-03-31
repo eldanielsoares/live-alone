ca880533782fa647a386b98b75a78e600aac251a
eldanielsoares <danielsoares7717@gmail.com>
2022-03-31 15:07:12 -0300

    feat add monthly spents migrations and entity

---
+++ src/modules/monthly_spents/infra/typeorm/entities/MonthlySpents.ts
@@ -0,0 +1,40 @@
+import User from '@modules/users/infra/typeorm/entities/User';
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity('monthly_spents')
+class MonthlySpents {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  name: string;
+
+  @Column()
+  value: number;
+
+  @Column()
+  type: string;
+
+  @Column()
+  user_id: string;
+
+  @ManyToOne(() => User)
+  @JoinColumn({ name: 'user_id' })
+  user: User;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+}
+
+export default MonthlySpents;
---
+++ src/shared/infra/typeorm/migrations/1648748990985-CreateMonthlySpents.ts
@@ -0,0 +1,72 @@
+import {
+  MigrationInterface,
+  QueryRunner,
+  Table,
+  TableForeignKey,
+} from 'typeorm';
+
+export default class CreateMonthlySpents1648748990985
+  implements MigrationInterface
+{
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'monthly_spents',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true,
+            generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
+          },
+          {
+            name: 'name',
+            type: 'varchar',
+          },
+          {
+            name: 'value',
+            type: 'decimal',
+          },
+          {
+            name: 'type',
+            type: 'varchar',
+          },
+
+          {
+            name: 'user_id',
+            type: 'uuid',
+          },
+
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+        ],
+      }),
+    );
+
+    await queryRunner.createForeignKey(
+      'monthly_spents',
+      new TableForeignKey({
+        name: 'UserSpent',
+        columnNames: ['user_id'],
+        referencedColumnNames: ['id'],
+        referencedTableName: 'users',
+        onDelete: 'SET NULL',
+        onUpdate: 'CASCADE',
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropForeignKey('monthly_spents', 'UserSpent');
+    await queryRunner.dropTable('monthly_spents');
+  }
+}
