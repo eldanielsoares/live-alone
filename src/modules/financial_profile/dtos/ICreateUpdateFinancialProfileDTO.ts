ca83afccb93701dfca3f3c508df9d2a0e2b8dad9
eldanielsoares <danielsoares7717@gmail.com>
2022-04-02 14:12:37 -0300

    feat new dto layer

---
+++ src/modules/financial_profile/dtos/ICreateUpdateFinancialProfileDTO.ts
@@ -0,0 +1,7 @@
+export default interface ICreateUpdateFinancialProfileDTO {
+  monthly_income: number;
+  monthly_spent: number;
+  job_type: string;
+  current_emergency_reserve: string;
+  user_id: string;
+}
