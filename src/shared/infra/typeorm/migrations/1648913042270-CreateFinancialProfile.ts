c97489aab46720f9f168fd5c756ba376aba40b82
eldanielsoares <danielsoares7717@gmail.com>
2022-04-02 14:11:16 -0300

    feat new financial profile migration

---
+++ src/shared/infra/typeorm/migrations/1648913042270-CreateFinancialProfile.ts
@@ -0,0 +1,74 @@
+import {
+  MigrationInterface,
+  QueryRunner,
+  Table,
+  TableForeignKey,
+} from 'typeorm';
+
+export default class CreateFinancialProfile1648913042270
+  implements MigrationInterface
+{
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'financial_profile',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true,
+            generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
+          },
+          {
+            name: 'monthly_income',
+            type: 'decimal',
+          },
+          {
+            name: 'monthly_spent',
+            type: 'decimal',
+          },
+          {
+            name: 'job_type',
+            type: 'varchar',
+          },
+          {
+            name: 'current_emergency_reserve',
+            type: 'decimal',
+          },
+          {
+            name: 'user_id',
+            type: 'uuid',
+          },
+
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+        ],
+      }),
+    );
+
+    await queryRunner.createForeignKey(
+      'financial_profile',
+      new TableForeignKey({
+        name: 'UserFinancialProfile',
+        columnNames: ['user_id'],
+        referencedColumnNames: ['id'],
+        referencedTableName: 'users',
+        onDelete: 'SET NULL',
+        onUpdate: 'CASCADE',
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('financial_profile');
+  }
+}
