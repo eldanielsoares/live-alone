2ff2055fc76e7a744dcd9596d63e5a0bc0e1934b
eldanielsoares <danielsoares7717@gmail.com>
2022-04-02 15:17:19 -0300

    feat ensure user can create only one financial profile

--- src/modules/financial_profile/services/CreateFinancialProfileService.ts
+++ src/modules/financial_profile/services/CreateFinancialProfileService.ts
@@ -1,4 +1,5 @@
 /* eslint-disable no-empty-function */
+import AppError from '@shared/errors/AppError';
 import { inject, injectable } from 'tsyringe';
 import FinancialProfile from '../infra/typeorm/entities/FinancialProfile';
 import IFinancialProfileRepository from '../repositories/IFinancialProfileRepository';
@@ -19,6 +20,12 @@ class CreateFinancialProfileService {
   ) {}
 
   public async execute(data: IRequest): Promise<FinancialProfile> {
+    const findFinancialProfile =
+      await this.financialProfileRepository.findByUserId(data.user_id);
+
+    if (findFinancialProfile) {
+      throw new AppError('Financial Profile already exists');
+    }
     const financialProfile = await this.financialProfileRepository.create(data);
     return financialProfile;
   }
